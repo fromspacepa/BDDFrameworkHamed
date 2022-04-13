765a9d857462387b12f490cbb0e6a79a763d90bc
Turtle$2021 <fhamed932@gmail.com>
2022-04-13 18:56:35 -0400

    updated

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import core.Base;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -21,7 +22,7 @@ public class LoginSteps extends Base { // Note to be noted: In this "LoginPageSt
 		login.clickOnMyAccountButton();
 		logger.info("My account button was clicked successfully");
 	}
-	@Then ("user click on login button")
+	@And ("user click on login button")
 	public void user_click_on_login_button() {
 		login.clickOnLoginButton();
 		logger.info("Login button was clicked successfully");
@@ -32,7 +33,7 @@ public class LoginSteps extends Base { // Note to be noted: In this "LoginPageSt
 		login.enterPassword();
 		logger.info("Username and Password entered successfully");
 	}
-	@Then ("user click on login button to login to the page")
+	@And ("user click on login button to login to the page")
 	public void user_click_on_login_button_to_login_to_the_page() {
 		login.clickOnLoginButtonMain();
 		logger.info("Login button was clicked successfully");
